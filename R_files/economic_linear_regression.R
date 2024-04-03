43cf2c9ce55d2dba2a6e083a8620067c5857d151
bennicholson2 <nicholson.benjaminn@gmail.com>
2024-04-02 22:28:43 -0400

    linear regression

--- R_files/economic_linear_regression.R
+++ R_files/economic_linear_regression.R
@@ -1,8 +1,1 @@
-total_car_sales <- cars_sold_economic_socioeconomic_data
-value_total_car_sales <- value_cars_sold_economic_socioeconomic_data
-
-total_car_sales$maryland_population <- as.integer(as.character(total_car_sales$maryland_population))
-total_car_sales$total_workforce <- as.integer(as.character(total_car_sales$total_workforce))
-
-value_total_car_sales$maryland_population <- as.integer(as.character(value_total_car_sales$maryland_population))
-value_total_car_sales$total_workforce <- as.integer(as.character(value_total_car_sales$total_workforce))
+data <- monthly_car_sales_all_data
